7729089fb68ef883a49f619eedbf7c327ec550bf
Łukasz Szumilas <lukaszszumilas9@wp.pl>
2020-10-09 09:33:33 +0200

    Remove duplicated directory

--- README.md
+++
@@ -1,7 +0,0 @@
-# LearningThroughDoing
-
-Some projects,games etc That i wrote to develop my coding skills.
-
-## What i Learned?
-
-
--- automateboringstuff/PatternMatchingWIthRegularExpressions/RegexObjects.py
+++
@@ -1,9 +0,0 @@
-#This is much shorter version of checkIfPhoneNumbers programs, using re module that provides finding patterns
-
-import re
-
-phoneNumRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
-
-mo = phoneNumRegex.search('My number is 415-555-4242.')
-
-print('Phone number found: ' + mo.group())
